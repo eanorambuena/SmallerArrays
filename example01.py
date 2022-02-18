338527c943eb1e4c9797a66b224d68fdd918541a
eanorambuena <eanorambuena@uc.cl>
2022-02-18 12:05:25 -0300

    docs(Root): Improve readability of examples & Add example01 to README

--- example01.py
+++ example01.py
@@ -5,8 +5,8 @@ from pysa import IntArray
 a = IntArray(10)
 b = [1] * 10000
 
-for e in b:
-    a.append(e)
+for element in b:
+    a.append(element)
 
 print("IntArray memory usage:", a.get_size())
 print("Builtin List memory usage:", sys.getsizeof(b))
