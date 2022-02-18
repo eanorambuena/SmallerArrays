9ed917d8d460b7eae45640ba8ba94822c9943695
eanorambuena <eanorambuena@uc.cl>
2022-02-17 21:43:14 -0300

    feat(Root): Add to_string method to SmallArray

--- pysa/generic.py
+++ pysa/generic.py
@@ -1,9 +1,9 @@
-import string
 from pysa.int import IntArray
+from pysa.utils import binary_to_string
 
 class SmallArray(IntArray):
 
-    def __init__(self, size, type = string):
+    def __init__(self, size, type = str):
         super().__init__(size)
         self.type = type
 
@@ -11,3 +11,6 @@ class SmallArray(IntArray):
         str_element = str(element)
         bin_element = "".join(format(ord(c), 'b') for c in str_element)
         super().append(int(bin_element))
+
+    def to_string(self):
+        return binary_to_string(self)
