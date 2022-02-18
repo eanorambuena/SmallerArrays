2bbbe350f66c21102a11946ad742cfc628f042e4
eanorambuena <eanorambuena@uc.cl>
2022-02-18 12:33:47 -0300

    docs(Root): Add Example Metrics to examples and README

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Smaller Arrays Implementations fully built in python 3.8
 
 ![example01](src/example01.png)
+![example01_metrics](src/example01_metrics.png)
 ![example02](src/example02.png)
+![example02_metrics](src/example02_metrics.png)
--- example01.py
+++ example01.py
@@ -3,12 +3,17 @@ import sys
 from pysa import IntArray
 
 array = IntArray(10)
-builtin_list = [1] * 10000
+builtin_list = [100, 200, 300, 400, 500]
 
 for element in builtin_list:
     array.append(element)
 
-print("IntArray memory usage:", array.get_size())
-print("Builtin list memory usage:", sys.getsizeof(builtin_list))
+print(f"""
+IntArray: {array}
+IntArray memory usage: {array.get_size()}
+
+Built-in list: {builtin_list}
+Built-in list memory usage: {sys.getsizeof(builtin_list)}
+""")
 
 print(array[4])
