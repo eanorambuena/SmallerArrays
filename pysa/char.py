b096d0b360d042a44c1a77a2a856498de140f161
eanorambuena <eanorambuena@uc.cl>
2022-02-18 13:35:37 -0300

    feat(Root): Add String class & set the items_size to 8 (8bit chars)

--- pysa/char.py
+++ pysa/char.py
@@ -3,9 +3,8 @@ from pysa.utils import binary_to_string
 
 class CharArray(IntArray):
 
-    def __init__(self, size, type = str):
-        super().__init__(size)
-        self.type = type
+    def __init__(self, items_size = 8, auto_resize = False):
+        super().__init__(items_size, auto_resize)
 
     def append(self, element):
         str_element = str(element)
@@ -14,3 +13,11 @@ class CharArray(IntArray):
 
     def to_string(self):
         return binary_to_string(self)
+
+class String(CharArray):
+    
+    def __init__(self, items_size = 8, auto_resize = False):
+        super().__init__(items_size, auto_resize)
+
+    def __str__(self):
+        return self.to_string()
