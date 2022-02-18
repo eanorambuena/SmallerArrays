56907ddbdcae4699b4aa9d26b20e9115fdab5f2e
eanorambuena <eanorambuena@uc.cl>
2022-02-18 11:48:45 -0300

    refactor(Root): Improve readability

--- pysa/int.py
+++ pysa/int.py
@@ -7,6 +7,12 @@ class IntArray():
         self.items = 0
         self.auto_grow_state = auto_grow
 
+    def __getitem__(self, index):
+        return self.list[index]
+
+    def __str__(self):
+        return str(self.list)
+
     def auto_grow(self, item):
 
         if self.auto_grow_state:
@@ -26,27 +32,27 @@ class IntArray():
         string = str(self.array)
         factor = self.size - (len(string) % self.size)
         result = "0" * (factor % self.size) + string
+
         if result == "0" * self.size and self.empty:
             return []
+
         list_result = []
+
         for i in range(len(result) // self.size):
             lower = i * self.size
             upper = (i + 1) * self.size
+
             sub = result[lower : upper]
+
             list_result.append(int(sub))
+
         return list_result
-        
-    def __str__(self):
-        return str(self.list)
         
     def pop(self):
         if len(str(self)) <= self.size:
             self.empty = True
         self.array = self.array // (10 ** self.size)
         self.items -= 1
-        
-    def __getitem__(self, index):
-        return self.list[index]
 
     def get_size(self):
         import sys
