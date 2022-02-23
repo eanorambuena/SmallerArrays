26a9fd9606499df929089f39425510e4889e1563
eanorambuena <eanorambuena@uc.cl>
2022-02-23 13:55:08 -0300

    fix(Root): fix(Root): Resolve Infinite loop bug made by the empty str value of str_number var

--- pysa/utils.py
+++ pysa/utils.py
@@ -1,14 +1,29 @@
 def binary_to_ascii(binary_item):
     """
-    Convert a binary string to an ASCII char.
+    Convert a binary number to an ASCII char.
     """
     str_number = str(binary_item)
+
+    if str_number == '':
+        return -1
+    
     decimal_number = int(str_number, 2)
     decoded = chr(decimal_number)
     return decoded
 
-def binary_to_string(binary_list):
+def binary_list_to_string(binary_list):
     """
-    Convert a binary string to an ASCII string.
+    Convert a binary list to an ASCII string.
     """
-    return "".join([binary_to_ascii(binary_ch) for binary_ch in binary_list])
+    string = ''
+
+    for binary_ch in binary_list:
+        result = binary_to_ascii(binary_ch)
+
+        if result == -1:
+            break
+
+        else:
+            string += result
+
+    return string
